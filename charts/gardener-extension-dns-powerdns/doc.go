30f0f5b0e3a350fbb674984eea1b952f4d86801f
Simon Mayer <simon.mayer@x-cellent.com>
2024-09-03 12:46:36 +0200

    Bump github.com/gardener/gardener to 1.85.5

--- charts/gardener-extension-dns-powerdns/doc.go
+++ charts/gardener-extension-dns-powerdns/doc.go
@@ -3,7 +3,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate sh -c "../../vendor/github.com/gardener/gardener/hack/generate-controller-registration.sh powerdns . $(cat ../../VERSION) ../../example/controller-registration.yaml DNSRecord:powerdns"
+//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh powerdns . $(cat ../../VERSION) ../../example/controller-registration.yaml DNSRecord:powerdns"
 
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
